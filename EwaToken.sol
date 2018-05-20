6e4bf93f7c100d10523bb82b55e70a95d2983c7b
realChainLife <realchainlife@outlook.com>
2018-05-20 14:04:05 +0300

    update function descriptions and fixes

--- EwaToken.sol
+++ EwaToken.sol
@@ -74,7 +74,7 @@ contract StandardToken is EwaToken {
         balances[msg.sender] -= _value;
         balances[_to] += _value;
 
-        Transfer(msg.sender, _to, _value);
+        emit Transfer(msg.sender, _to, _value);
 
         return true;
     }
